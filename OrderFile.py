5f65696dcb0cc30b10aab03b0dec7f58bd7bb1e8
Claude J Schmit <cjs213@demeter.ph.ic.ac.uk>
2015-09-28 14:36:12 +0100

    Now orderfile ignores empty lines

--- OrderFile.py
+++ OrderFile.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import operator
 
 filename_in = str(sys.argv[1])
 if len(sys.argv) == 3:
@@ -7,9 +8,14 @@ if len(sys.argv) == 3:
 else:
     filename_out = filename_in
 
-
+def checkline(line):
+    if (line == "\n") or (line == ""):
+        return False
+    else:
+        return True
+    
 with open(filename_in, "r") as f:
-    sorted_file = sorted(f)
+    sorted_file = sorted(filter(checkline,f))
 
 with open(filename_out, "w+") as f:
     for line in sorted_file:
