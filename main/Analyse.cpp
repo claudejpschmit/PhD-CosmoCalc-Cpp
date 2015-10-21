f2a74744bb2540af1df001642fe5619713e3ecee
Claude J Schmit <cjs213@demeter.ph.ic.ac.uk>
2015-10-21 12:55:19 +0100

    Updated analyse to read the run number from the command line.

--- main/Analyse.cpp
+++ main/Analyse.cpp
@@ -13,10 +13,21 @@ using namespace alglib;
 
 int main(int argc, char* argv[])
 {
-    vector<string> keys = {"ombh2", "omch2", "hubble","fesc", "fstar"};
+    int run_number;
+    if (argc < 2)
+        run_number = 11;
+    else 
+        run_number = atoi(argv[1]);
+    stringstream prefix;
+    if (run_number < 10)
+        prefix << "0" << run_number;
+    else 
+        prefix << run_number;
+
+    vector<string> keys = {"ombh2", "omch2", "hubble", "fesc", "fstar"};
     Analyser analyse;
-    Fisher_return_pair finv = analyse.build_Fisher_inverse(keys, "11", "output/Fisher/");
-    analyse.draw_error_ellipses(finv, keys, 11, "output/Fisher/");
+    Fisher_return_pair finv = analyse.build_Fisher_inverse(keys, prefix.str(), "output/Fisher/");
+    analyse.draw_error_ellipses(finv, keys, run_number, "output/Fisher/");
 
     return 0;
 }
