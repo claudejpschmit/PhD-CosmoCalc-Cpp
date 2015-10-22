bb757d6a943fe198132c4d0210d83d3c2f56736d
Claude J Schmit <cjs213@demeter.ph.ic.ac.uk>
2015-10-22 12:00:21 +0100

    Commenting a bunch

--- include/FisherClass.hpp
+++ include/FisherClass.hpp
@@ -13,45 +13,225 @@ using namespace arma;
 
 class Fisher {
     public:
-        Fisher(map<string, double> params, string Fl_filename, vector<string> params_keys_considered);
+        /**
+         * Constructor for the Fisher object. 
+         * This constructor initializes the following objects:
+         *      - the CALC pointer, which initializes the CosmoCalc object to 
+         *        be used to calculate the Cl's.
+         *      - the fiducial parameters of the simulation.
+         *      - the parameters that are to be varied by the run
+         *      - stores information about whether RSD and Noise are to be 
+         *        calculated.
+         * @param params is a dictionary that determines the initial/fiducial
+         *          values of the parameters of the run.
+         * @param Fl_filename determines the file into which the sum over all
+         *          the Fl's are to be written to. TODO: Check whether this is 
+         *          necessary or not.
+         * @param params_keys_considered is a list with the parameter keys that
+         *          are to be varied in the run.
+         */
+        Fisher(map<string, double> params, string Fl_filename,\
+                vector<string> params_keys_considered);
+
+        /**
+         * The destructor releases the CALC pointer as well as closing the
+         * Fl_file.
+         */
         ~Fisher();
 
-        void update_Model(map<string, double> new_params, int *Pk_index, int *Tb_index, int *q_index);
-        mat compute_Cl(int l, int Pk_index, int Tb_index, int q_index, vector<double> krange);
+        /**
+         * This function updates the model used to perform the calculations
+         * given a set of new parameters. It updates:
+         *      - comoving distance q(z),
+         *      - the power spectrum Pk(z),
+         *      - and the G21 function Tb(z).
+         * @param new_params contains the dictionary of new parameters that
+         *          the model should be updated to.
+         * @param Pk_index points to the index this new model should be 
+         *          identified as for the matter power spectrum.
+         * @param Tb_index points to the index this new model should be 
+         *          identified as for the 21cm signal.
+         * @param Pk_index points to the index this new model should be 
+         *          identified as for the comoving distance.
+
+         */
+        void update_Model(map<string, double> new_params, int *Pk_index,\
+                int *Tb_index, int *q_index);
+
+        /**
+         * This function computes a covariance matrix containing all the 
+         * Cl(k1,k2)'s for a given model, set of (k1,k2)'s and l. 
+         * If this exact covariance matrix has already been calculated
+         * before, the function finds the file where it was stored and just
+         * reads it from there. Otherwise it calculates it and stores it. 
+         *
+         * @param l is the angular mode for which the Covariance matrix is 
+         *          calculated.
+         * @param Pk_index is the matter power spectrum model index used.
+         * @param Tb_index is the 21cm signal model index used.
+         * @param q_index is the comoving distance model index used.
+         * @param krange is a list of k's that determine the modes which should
+         *          be included in the covariance matrix.
+         * @return Returns an aramdillo mat object, which is the covariance 
+         *          matrix considering a certain model as a function of the
+         *          angular mode l.
+         */
+        mat compute_Cl(int l, int Pk_index, int Tb_index, int q_index,\
+                vector<double> krange);
         
+        /**
+         * 
+         */
         double Cl_derivative(int l, string param_key, double k1, double k2,\
                 int *Pk_index, int *Tb_index, int *q_index);
-        double Cl_loglog_derivative(int l, string param_key, double k1, double k2,\
-                int *Pk_index, int *Tb_index, int *q_index);
+        /**
+         *
+         */
+        double Cl_loglog_derivative(int l, string param_key, double k1,\
+                double k2, int *Pk_index, int *Tb_index, int *q_index);
 
+        /**
+         * This function computes the derivative of the covariance matrix e 
+         * with respect to some parameter. A four point derivative is used.
+         * If this exact derivative matrix has already been calculated
+         * before, the function finds the file where it was stored and just
+         * reads it from there. Otherwise it calculates it and stores it. 
+         *
+         * @param l is the angular mode for which the Covariance matrix is 
+         *          calculated.
+         * @param param_key is the parameter key identifying the parameter 
+         *          respect to which the derivative is to be taken.
+         * @param Pk_index points to the matter power spectrum model index.
+         * @param Tb_index points to the 21cm signal model index.
+         * @param q_index points to the comoving distance model index.
+         * @param krange is a list of k's that determine the modes which should
+         *          be included in the covariance matrix.
+         * @return Returns an aramdillo mat object, which is the derivative 
+         *          matrix considering a certain model as a function of the
+         *          angular mode l.
+         */
         mat Cl_derivative_matrix(int l, string param_key, int *Pk_index,\
                 int *Tb_index, int *q_index, vector<double> krange);
-            
-        double compute_Fl(int l, string param_key1, string param_key2, int ksteps_Cl,\
-                double *cond_num, int *Pk_index, int *Tb_index, int *q_index);
-        double compute_Fl(int l, string param_key1, string param_key2, double kstepsize,\
-                double *cond_num, int *Pk_index, int *Tb_index, int *q_index);
+        
+        /**
+         *
+         */
+        double compute_Fl(int l, string param_key1, string param_key2,\ 
+                int ksteps_Cl, double *cond_num, int *Pk_index, int *Tb_index,\
+                int *q_index);
+        /**
+         *
+         */
+        double compute_Fl(int l, string param_key1, string param_key2,\
+                double kstepsize, double *cond_num, int *Pk_index,\
+                int *Tb_index, int *q_index);
+        /**
+         * This function initializes the necessary interpolator objects needed
+         * for the run. This is necessary so that the interpolators are ready 
+         * when needed from each thread. Otherwise, each thread would start 
+         * calculating the interpolators itself, which is undesirable and
+         * can easily lead to bugs.
+         *
+         * @param param_key is the key of the parameter that is to be varied
+         *          in the run.
+         * @param Pk_index points to the matter power spectrum model index.
+         * @param Tb_index points to the 21cm signal model index.
+         * @param q_index points to the comoving distance model index.
+         */
+        void initializer(string param_key, int *Pk_index, int *Tb_index,\
+                int *q_index);
 
-        void initializer(string param_key, int *Pk_index, int *Tb_index, int *q_index);
-
+        /**
+         *
+         */
         double F(string param_key1, string param_key2);
+        
+        /**
+         *
+         */
         double F_fixed_kstepsize(string param_key1, string param_key2);
-        double F_fixed_kstepsize(int lmin, int lmax, int n_points_per_thread, int n_threads);
+        
+        /**
+         *
+         */
+        double F_fixed_kstepsize(int lmin, int lmax, int n_points_per_thread,\
+                int n_threads);
+        
+        /**
+         *
+         */
+        void Fl_varying_ksteps(int l, string param_key1,\
+                string param_key2, int min_ksteps_Cl, int max_ksteps_Cl,\
+                int ksteps_spacing);
 
-        void Fl_varying_ksteps(int l, string param_key1, string param_key2, int min_ksteps_Cl,\
-                int max_ksteps_Cl, int ksteps_spacing);
-        void Fl_varying_ksteps_smart(int l, string param_key1, string param_key2, int min_ksteps_Cl,\
-                int max_ksteps_Cl);
-
+        /**
+         *
+         */
+        void Fl_varying_ksteps_smart(int l, string param_key1,\
+                string param_key2, int min_ksteps_Cl, int max_ksteps_Cl);
+        
+        /**
+         * This function writes a matrix to a binary file.
+         *
+         * @param matrix is the matrix that is to be written to file.
+         * @param filename is the name of the file to which the matrix is 
+         *          to be written.
+         */
         void write_matrix(mat matrix, string filename);
+        
+        /**
+         * This function reads a matrix from a binary file.
+         *
+         * @param filename is the name of the file from which the matrix is
+         *          read.
+         * @param n_rows is the number of rows in the matrix.
+         * @param n_cols is the number of columns in the matrix.
+         *
+         * @return Returns an armadillo matrix object of the matrix read.
+         */
         mat read_matrix(string filename, int n_rows, int n_cols);
+        
+        /**
+         * This function checks whether a file with a given name already
+         * exists or not.
+         *
+         * @param filename is the name of the file to be checked.
+         *
+         * @return Returns true if the file exists and false if not.
+         */
         bool check_file(string filename);
+        
+        /**
+         * This function records the run parameters to a file called 
+         * RUN_INFO.dat so that one can later identify which parameters
+         * were used in each run.
+         *
+         * @param lmin is the minimum l mode considered in the run.
+         * @param lmax is the maximal l mode considered in the run.
+         * @param lstepsize is the stepsize between adjacent l modes.
+         * @param kstepsize is the stepsize between adjacent k modes.
+         *
+         * @return Returns filename prefix to be used to store resulting 
+         *          Fl values for this run.
+         */
         string update_runinfo(int lmin, int lmax,\
                 int lstepsize, double kstepsize);
 
     private:
+        // **** Functions **** //
+
+        /**
+         *
+         */
         vector<double> give_kmodes(int l, double kmax, int steps);
+        
+        /**
+         *
+         */
         vector<double> give_kmodes(int l, double k_max, double kstepsize);
+
+        // **** Variables **** //
+
         vector<string> model_params_keys;
         CosmoCalc *CALC;
         ofstream Fl_file;
--- include/Helper.hpp
+++ include/Helper.hpp
@@ -9,16 +9,33 @@ using namespace std;
 using namespace alglib;
 using namespace arma;
 
+/**
+ * Pk_interpolator includes a value for each parameter affecting the power
+ * spectrum Pk(z), and an interpolation object corresponding to those 
+ * parameter values.
+ */
 struct Pk_interpolator {
     double ombh2, omnuh2, omch2, omk, hubble, tcmb, w_DE;
     spline2dinterpolant interpolator;
 };
 
+/**
+ * Tb_interpolator includes a value for each parameter affecting the 21cm
+ * brightness temperature fluctuations Tb(z), and an interpolation object 
+ * corresponding to those parameter values. This version is to be used
+ * with the Global21cm Code.
+ */
 struct Tb_interpolator {
-    double ombh2, omnuh2, omch2, omk, hubble, s8, T_CMB, n_s,w_DE, fstar,fesc,nion,fx,flya;
+    double ombh2, omnuh2, omch2, omk, hubble, s8, T_CMB, n_s,w_DE,\
+        fstar, fesc, nion, fx, flya;
     spline1dinterpolant interpolator;
 };
 
+/**
+ * q_interpolator includes a value for each parameter affecting the comoving
+ * distance r(z) = q(z), and an interpolation object corresponding to those 
+ * parameter values for both the distance as well as the Hubble function.
+ */
 struct q_interpolator {
     double ombh2, omnuh2, omch2, omk, hubble, t_cmb, w_DE;
     double h;
@@ -26,17 +43,32 @@ struct q_interpolator {
     spline1dinterpolant interpolator_Hf;
 };
 
+/**
+ * Tb_analytic_interpolator includes a value for each parameter affecting the
+ * 21cm brightness temperature fluctuations Tb(z), and an interpolation object 
+ * corresponding to those parameter values. This version is for the analytic
+ * determination of Tb(z).
+ */
 struct Tb_analytic_interpolator {
     double ombh2, omnuh2, omch2, hubble, t_cmb;
     spline1dinterpolant interpolator;
 };
+
+/**
+ * A Fisher_return pair includes a matrix of Fisher values, as well as a
+ * matrix of parameter key pairs so to identify which matrix elements 
+ * correspond to which parameters.
+ */
 
 struct Fisher_return_pair {
     mat matrix;
     vector<vector<vector<string>>> matrix_indecies;
 };
 
-// I need x,y coordinates for the centre as well...
+/**
+ * The ellipse structure includes all necessary information to define an 
+ * ellipse.
+ */
 struct Ellipse {
     double a2, b2, theta, cx, cy;
 };
